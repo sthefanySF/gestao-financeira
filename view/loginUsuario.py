0768dfc8fc62d0496417d614413b81c462e2530d
emillyvsl <emillysilva.18@outlook.com>
2023-08-13 19:20:31 -0500

    Modificações

--- view/loginUsuario.py
+++ view/loginUsuario.py
@@ -4,49 +4,62 @@ from ttkbootstrap.constants import *
 from PIL import Image, ImageTk
 import webbrowser
 
+from telaInicial import TelaInicial
+
 def open_link(event):
     webbrowser.open("https://www.example.com")  # Substitua pelo link real
 
-janela = ttk.Window(themename='litera')
-janela.geometry('700x400')
-# GestãoFácil
-janela.title('Gestão Fácil/Login')
+class Login:
+   
+    def entrar(self):
+        tela = TelaInicial(self._janela)  # Cria uma instância da classe Tela
 
-parte_verde = tk.Label(janela, background='#33bc7d')
 
-image = Image.open(r"C:\Users\Emilly\Desktop\gestao-financeira\logo (4).png")  # Substitua pelo caminho real da imagem
-photo = ImageTk.PhotoImage(image)
-image_label = tk.Label(parte_verde, image=photo, bg='#33bc7d',width=450)
-image_label.pack()
+    def __init__(self):
+        self._janela = ttk.Window(themename='litera')
+        self._janela.geometry('700x400')
+        self._janela.title('Gestão Fácil/Login')
 
-parte_verde.grid(row=0, column=0, rowspan=4, sticky="w")
+        self._parte_verde = tk.Label(self._janela, background='#33bc7d')
 
-frame_login = ttk.Frame(janela)
-frame_login.grid(row=0, column=1)
+        image = Image.open(r"C:\Users\Emilly\Desktop\gestao-financeira\logo (4).png")  # Substitua pelo caminho real da imagem
+        photo = ImageTk.PhotoImage(image)
+        self._image_label = tk.Label(self._parte_verde, image=photo, bg='#33bc7d', width=450)
+        self._image_label.pack()
 
-lbl_logo = ttk.Label(frame_login, text='Entre ou crie sua conta')
-lbl_logo.config(font="Arial 10 bold")
-lbl_logo.grid(row=0, column=2, columnspan=4, pady=50)
+        self._parte_verde.grid(row=0, column=0, rowspan=4, sticky="w")
 
-lbl_email = ttk.Label(frame_login, text='Email:', width=30).grid(row=1, column=3, sticky="e")
-etr_email = ttk.Entry(frame_login, width=30).grid(row=2, column=3, sticky="e")
+        self._frame_login = ttk.Frame(self._janela)
+        self._frame_login.grid(row=0, column=1)
 
-lbl_senha = ttk.Label(frame_login, text='Senha:', width=30).grid(row=3, column=3, sticky="e")
-etr_senha = ttk.Entry(frame_login, width=30).grid(row=4, column=3, sticky="e")
+        self._lbl_logo = ttk.Label(self._frame_login, text='Entre ou crie sua conta')
+        self._lbl_logo.config(font="Arial 10 bold")
+        self._lbl_logo.grid(row=0, column=2, columnspan=4, pady=50)
 
-link_label = ttk.Label(frame_login, text="esqueci a senha", cursor="hand2", font=("Helvetica", 8, "underline"))
-link_label.grid(row=5, column=3, sticky="e")  # Centraliza na coluna da direita
-link_label.bind("<Button-1>", open_link)
+        self._lbl_email = ttk.Label(self._frame_login, text='Email:', width=30).grid(row=1, column=3, sticky="e")
+        self._etr_email = ttk.Entry(self._frame_login, width=30).grid(row=2, column=3, sticky="e")
 
-btn = ttk.Button(frame_login, text='Entrar', width=20,bootstyle="success").grid(row=6, column=3, pady=10)
-btn_cadastrar = ttk.Button(frame_login, text='Cadastar',bootstyle="success-outline").grid(row=7, column=3, pady=10)
+        self._lbl_senha = ttk.Label(self._frame_login, text='Senha:', width=30).grid(row=3, column=3, sticky="e")
+        self._etr_senha = ttk.Entry(self._frame_login, width=30).grid(row=4, column=3, sticky="e")
 
-# Configuração das colunas e linhas do grid
-janela.grid_columnconfigure(0, weight=1)
-janela.grid_columnconfigure(1, weight=1)
-janela.grid_rowconfigure(0, weight=1)
-janela.grid_rowconfigure(1, weight=1)
-janela.grid_rowconfigure(2, weight=1)
-janela.grid_rowconfigure(3, weight=1)
+        self._link_label = ttk.Label(self._frame_login, text="esqueci a senha", cursor="hand2", font=("Helvetica", 8, "underline"))
+        self._link_label.grid(row=5, column=3, sticky="e")  # Centraliza na coluna da direita
+        self._link_label.bind("<Button-1>", open_link)
 
-janela.mainloop()
+        self._btn = ttk.Button(self._frame_login, text='Entrar', width=20, bootstyle="success", command=self.entrar)
+        self._btn.grid(row=6, column=3, pady=10)
+
+        self._btn_cadastrar = ttk.Button(self._frame_login, text='Cadastar', bootstyle="success-outline").grid(row=7, column=3, pady=10)
+
+        # Configuração das colunas e linhas do grid
+        self._janela.grid_columnconfigure(0, weight=1)
+        self._janela.grid_columnconfigure(1, weight=1)
+        self._janela.grid_rowconfigure(0, weight=1)
+        self._janela.grid_rowconfigure(1, weight=1)
+        self._janela.grid_rowconfigure(2, weight=1)
+        self._janela.grid_rowconfigure(3, weight=1)
+
+        self._janela.mainloop()
+
+if __name__ == "__main__":
+    login = Login()
---
+++ view/telaInicial.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
+from PIL import Image, ImageTk
+import webbrowser
+
+class TelaInicial:
+    def __init__(self, janela2):
+        self._janela = janela2
+        self._janela.geometry('700x500')
+        self._janela.title('Gestão Fácil/Tela inicial')
+
+        frame_menu = ttk.Frame(self._janela)
+        frame_menu.grid(row=0, column=0)
+        margin_menu = 3
+        self._btn_perfil = ttk.Button(frame_menu, text='Meu perfil', width=20, bootstyle="success")
+        self._btn_perfil.grid(row=0, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._btn_inicio = ttk.Button(frame_menu, text='Inicio', width=20, bootstyle="success")
+        self._btn_inicio.grid(row=1, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._btn_regis = ttk.Button(frame_menu, text='Registrar Transação', width=20, bootstyle="success")
+        self._btn_regis.grid(row=2, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._btn_extrato = ttk.Button(frame_menu, text='Extrato', width=20, bootstyle="success")
+        self._btn_extrato.grid(row=3, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._btn_contas = ttk.Button(frame_menu, text='Minhas contas', width=20, bootstyle="success")
+        self._btn_contas.grid(row=4, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._btn_voltar = ttk.Button(frame_menu, text='Voltar', width=20, bootstyle="success")
+        self._btn_voltar.grid(row=5, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
+
+        self._lbl_meter = ttk.Label(self._janela, text='Resumo do mês')
+        self._lbl_meter.grid(row=0, column=4, columnspan=2)
+        self._lbl_meter.config(font="Arial 13 bold")
+
+        self._mtr1 = ttk.Meter(self._janela, subtext='Saldo', bootstyle='success', interactive=False, amountused=50)
+        self._mtr1.grid(row=1, column=4, rowspan=4, sticky='e', padx=20)
+
+        self._mtr2 = ttk.Meter(self._janela, subtext='Gastos', bootstyle='danger', interactive=False, amountused=20)
+        self._mtr2.grid(row=1, column=5, rowspan=4, sticky='e', padx=20)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    tela_inicial = TelaInicial(root)
+    root.mainloop()
