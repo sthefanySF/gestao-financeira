7dbd6fda6eafbc8d3abaa5c77a5eba8a6caffd71
SthefanySF <sthefanysantos0815@gmail.com>
2023-08-30 09:43:42 -0500

    extrato atualizado

--- view/extratos.py
+++ view/extratos.py
@@ -1,8 +1,13 @@
 import tkinter as tk
 import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
-from controller import usuario
 
+import sys
+
+sys.path.insert(0, './')
+sys.path.insert(0, './controller')
+from controller import usuario
+from controller.usuario import Usuario
 from perfilUsuario import PerfilUsuario
 from registrarTransacao import RegistrarTransacoes
 
@@ -11,7 +16,7 @@ class Extratos:
         self._id_usuario_atual = usuario
         self._janela = master
         self._janela.title('Gestão Fácil/Extratos')
-        self._janela.geometry('850x500')
+        self._janela.geometry('1030x800')
 
         # Frame principal para conter tudo
         frame_principal = ttk.Frame(self._janela)
@@ -30,17 +35,25 @@ class Extratos:
         frame_menu.grid(row=0, column=0, columnspan=2, sticky='ew')
         margin_menu = 3
 
-        # Adicione um novo Frame para "Resumo do mês"
+        # Frame para "Resumo do mês"
         frame_resumo = ttk.Frame(frame_menu)
         frame_resumo.grid(row=0, column=2, columnspan=5)
         frame_resumo.grid_rowconfigure(0, weight=1)
         frame_resumo.grid_columnconfigure(0, weight=1)
 
-        # Adicione um rótulo para "Resumo do mês" e centralize-o verticalmente com pack
         label_resumo = ttk.Label(frame_resumo, text='Resumo do mês', font=('Helvetica', 16))
         label_resumo.pack(expand=True, pady=10)
 
-        #botões do menu
+         #LabelFrame para Ganho do mês
+        self._lbl_ganho_mes = ttk.LabelFrame(frame_menu, text='Ganho do mês', bootstyle="success", width=400, height=200)
+        self._lbl_ganho_mes.grid(row=1, column=2, padx=10, pady=10, sticky='ew', rowspan=5)
+
+        # LabelFrame para Gasto do mês
+        self._lbl_gasto_mes = ttk.LabelFrame(frame_menu, text='Gasto do mês', bootstyle="danger", width=400, height=200)
+        self._lbl_gasto_mes.grid(row=1, column=3, padx=10, pady=10, sticky='ew', rowspan=5)
+
+
+        # Botões do menu
         self._btn_perfil = ttk.Button(frame_menu, text='Meu perfil', width=20, bootstyle="success", command=self.abrir_perfil)
         self._btn_perfil.grid(row=0, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
 
@@ -59,66 +72,76 @@ class Extratos:
         self._btn_voltar = ttk.Button(frame_menu, text='Voltar', width=20, bootstyle="success", command=self.voltar) 
         self._btn_voltar.grid(row=5, column=0, sticky='w', pady=margin_menu, padx=margin_menu)
 
+        # LabelFrame para Ganhos para a tabela de ganhos
+        self._lbl_ganhos = ttk.LabelFrame(frame_menu_tabela, text='Ganhos', bootstyle="success", width=400, height=200)
+        self._lbl_ganhos.grid(row=1, column=0, padx=10, pady=10, sticky='ew')
 
-       #tabela
-        self._tabela = ttk.Treeview(frame_menu_tabela, columns=('Data', 'Descrição do gasto', 'Categoria', 'Valor do gasto'))
+        # Tabela de Ganhos
+        self._tabela_ganhos = ttk.Treeview(self._lbl_ganhos, columns=('Ganho Mensal', 'Ganho Adicional', 'Descrição do Ganho Adicional'))
 
-        #scrollbar
-        self._scrollbar = ttk.Scrollbar(frame_menu_tabela, orient='vertical', command=self._tabela.yview)
-        self._tabela.configure(yscrollcommand=self._scrollbar.set)
-        
-        # larguras das colunas
-        self._tabela.column('#0', width=0)
-        self._tabela.column('Data', width=100)
-        self._tabela.column('Descrição do gasto', width=100)
-        self._tabela.column('Categoria', width=100)
-        self._tabela.column('Valor do gasto', width=100)
+        # Larguras das colunas para a tabela de ganhos
+        self._tabela_ganhos.column('#0', width=0)
+        self._tabela_ganhos.column('Ganho Mensal', width=100)
+        self._tabela_ganhos.column('Ganho Adicional', width=100)
+        self._tabela_ganhos.column('Descrição do Ganho Adicional', width=200)
 
-        #cabeçalhos das colunas
-        self._tabela.heading('#0', text='', anchor=W)
-        self._tabela.heading('Data', text='Data', anchor=W)
-        self._tabela.heading('Descrição do gasto', text='Descrição do gasto', anchor=W)
-        self._tabela.heading('Categoria', text='Categoria', anchor=W)
-        self._tabela.heading('Valor do gasto', text='Valor do gasto', anchor=W)
+        # Cabeçalhos das colunas para a tabela de ganhos
+        self._tabela_ganhos.heading('#0', text='', anchor=W)
+        self._tabela_ganhos.heading('Ganho Mensal', text='Ganho Mensal', anchor=W)
+        self._tabela_ganhos.heading('Ganho Adicional', text='Ganho Adicional', anchor=W)
+        self._tabela_ganhos.heading('Descrição do Ganho Adicional', text='Descrição do Ganho Adicional', anchor=W)
 
-           # Posicione a tabela na janela
-        self._tabela.grid(row=1, column=0, sticky='nsew')
-        self._scrollbar.grid(row=1, column=1, sticky='ns')
+        # Scrollbar para a tabela de ganhos
+        self._scrollbar_ganhos = ttk.Scrollbar(self._lbl_ganhos, orient='vertical', command=self._tabela_ganhos.yview)
+        self._tabela_ganhos.configure(yscrollcommand=self._scrollbar_ganhos.set)
 
-        # LabelFrame para "Ganho do mês"
-        self._lbl_ganho = ttk.LabelFrame(frame_menu, text='Ganho do mês', bootstyle="success", width=400, height=200)
-        self._lbl_ganho.grid(row=2, column=2, padx=10, pady=10, sticky='ew')
+
+        self._tabela_ganhos.grid(row=0, column=0, sticky='nsew')
+        self._scrollbar_ganhos.grid(row=0, column=1, sticky='ns')
 
         
-        # LabelFrame para "Gasto do mês"
-        self._lbl_gasto = ttk.LabelFrame(frame_menu, text='Gasto do mês', bootstyle="danger", width=400, height=200)
-        self._lbl_gasto.grid(row=2, column=3, padx=10, pady=10, sticky='ew')
+        self._lbl_ganhos.grid_rowconfigure(0, weight=1)
+        self._lbl_ganhos.grid_columnconfigure(0, weight=1)
+
+        #LabelFrame para Gastos
+        self._lbl_gastos = ttk.LabelFrame(frame_menu_tabela, text='Gastos', bootstyle="danger", width=400, height=200)
+        self._lbl_gastos.grid(row=2, column=0, padx=10, pady=10, sticky='ew')
+
+        # Tabela de Gastos
+        self._tabela_gastos = ttk.Treeview(self._lbl_gastos, columns=('Data', 'Descrição do gasto', 'Categoria', 'Valor do gasto'))
 
         
+        self._tabela_gastos.column('#0', width=0)
+        self._tabela_gastos.column('Data', width=100)
+        self._tabela_gastos.column('Descrição do gasto', width=100)
+        self._tabela_gastos.column('Categoria', width=100)
+        self._tabela_gastos.column('Valor do gasto', width=100)
 
-        # Personalizável, apenas para teste
-        ttk.Label(self._lbl_ganho, text='Ganhos do mês vão aqui').pack()
-        ttk.Label(self._lbl_gasto, text='Gastos do mês vão aqui').pack()
+      
+        self._tabela_gastos.heading('#0', text='', anchor=W)
+        self._tabela_gastos.heading('Data', text='Data', anchor=W)
+        self._tabela_gastos.heading('Descrição do gasto', text='Descrição do gasto', anchor=W)
+        self._tabela_gastos.heading('Categoria', text='Categoria', anchor=W)
+        self._tabela_gastos.heading('Valor do gasto', text='Valor do gasto', anchor=W)
 
-        # Configurar redimensionamento da tabela
-        frame_menu_tabela.grid_rowconfigure(0, weight=0)  # Bloqueia a expansão do menu
-        frame_menu_tabela.grid_rowconfigure(1, weight=1)
+        # Scrollbar para a tabela de gastos
+        self._scrollbar_gastos = ttk.Scrollbar(self._lbl_gastos, orient='vertical', command=self._tabela_gastos.yview)
+        self._tabela_gastos.configure(yscrollcommand=self._scrollbar_gastos.set)
+
+        
+        self._tabela_gastos.grid(row=0, column=0, sticky='nsew')
+        self._scrollbar_gastos.grid(row=0, column=1, sticky='ns')
+
+       
+        self._lbl_gastos.grid_rowconfigure(0, weight=1)
+        self._lbl_gastos.grid_columnconfigure(0, weight=1)
+
+    
+        frame_menu_tabela.grid_rowconfigure(0, weight=1)
         frame_menu_tabela.grid_columnconfigure(0, weight=1)
-        frame_menu_tabela.grid_columnconfigure(1, weight=0)  # Bloqueia a expansão do scrollbar
 
-        # Configurar redimensionamento do frame_principal
-        frame_principal.grid_rowconfigure(0, weight=1)
-        frame_principal.grid_columnconfigure(0, weight=1)
-        
-        
     def abrir_perfil(self):
         perfil_window = PerfilUsuario(self._janela)
-    
-    def abrir_extratro(self):
-        self._janela_Extrato = tk.Toplevel(self._janela)
-
-        extrato_window = Extratos(self._janela_Extrato)
-        
     
     def abrir_transacoes(self):
         self._janela_transacoes = tk.Toplevel(self._janela)
